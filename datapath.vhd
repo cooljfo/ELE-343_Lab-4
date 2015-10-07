e58f801e4d73b82a4a7b0d5baba0c7dc523c196c
Jean-Francois <j.f_chenier@hotmail.com>
2015-10-07 17:05:09 -0400

    fix mais pas fonctionnel

--- datapath.vhd
+++ datapath.vhd
@@ -12,13 +12,13 @@ generic(
 port (clk                : in  std_logic;
       reset              : in  std_logic;
       MemtoReg           : in  std_logic;
-      PCSrc              : in  std_logic_vector(N downto 0)
+      PCSrc              : in  std_logic_vector(N downto 0);
       AluSrc             : in  std_logic;
       RegDst             : in  std_logic;
       RegWrite           : in  std_logic;
---      Jump               : in  std_logic;
+      Jump               : in  std_logic;
       AluControl         : in  std_logic_vector(3 downto 0);
---      Zero               : out std_logic;
+      Zero               : out std_logic;
       PC                 : out std_logic_vector(N downto 0);
       Instruction        : in  std_logic_vector(N downto 0);
       AluResult          : out std_logic_vector(N downto 0);
@@ -29,16 +29,6 @@ end Datapath;
 
 architecture DataPath_archi of DataPath is
 
-COMPONENT Controller
-PORT (OP, Funct                   : in std_logic_vector(5 downto 0);
-      Zero                        : in std_logic;
-      MemtoReg, MemWrite, MemRead : out std_logic;
-      PCSrc, AluSrc               : out std_logic;
-      RegDst, RegWrite            : out std_logic;
-      Jump                        : out std_logic;
-      AluControl                  : out std_logic_vector(3 downto 0)
-     );
-END COMPONENT; -- Controller;
 
 COMPONENT imem --single cycle MIPS processor
 PORT (aa: in std_logic_vector(5 downto 0);
@@ -81,36 +71,47 @@ PORT (
    zero: out std_logic); 								--	sortie pour savoir si le resultat vaut 0
 END COMPONENT;
 
-SIGNAL PCPlus4 std_logic_vector(31 downto 0);
-SIGNAL PCJump std_logic_vector(31 downto 0);
-SIGNAL PCNextbr std_logic_vector(31 downto 0);
-SIGNAL Jump std_logic;
-SIGNAL PCNext std_logic_vector(31 downto 0);
-SIGNAL SignImm std_logic_vector(31 downto 0);
-SIGNAL PCBranch std_logic_vector(31 downto 0);
-SIGNAL PCScr,ALUSrc std_logic;
-SIGNAL MemtoReg, MemWrite, MemRead std_logic;
-SIGNAL RegDst, RegWrite std_logic;
-SIGNAL AluControl std_logic_vector(3 downto 0);
-SIGNAL Zero std_logic;
-SIGNAL WriteReg std_logic;
-SIGNAL SrcA,rd2 std_logic_vector(31 downto 0);
-SIGNAL SrcB std_logic_vector(31 downto 0);
-SIGNAL Result std_logic;
+COMPONENT DFF
+generic(
+	 INPUT_LENGTH: integer := 31
+        );
+  Port ( D          : in  STD_LOGIC_VECTOR(INPUT_LENGTH DOWNTO 0);
+         clk        : in  STD_LOGIC;
+         reset      : in  STD_LOGIC; 
+         Q          : out STD_LOGIC_VECTOR(INPUT_LENGTH DOWNTO 0)
+       );
+END COMPONENT;
 
-SignImm <= std_logic_vector(resize(signed(Instruction(15 downto 0)), SignImm'length));
+SIGNAL PCPlus4: std_logic_vector(31 downto 0);
+SIGNAL PCJump: std_logic_vector(31 downto 0);
+SIGNAL PCNextbr: std_logic_vector(31 downto 0);
+SIGNAL PCNext: std_logic_vector(31 downto 0);
+SIGNAL SignImm: std_logic_vector(31 downto 0);
+SIGNAL PCBranch: std_logic_vector(31 downto 0);
+SIGNAL MemWrite: std_logic;
+SIGNAL WriteReg: std_logic;
+SIGNAL SrcA : std_logic_vector(31 downto 0);
+SIGNAL rd2 : std_logic_vector(31 downto 0);
+SIGNAL SrcB: std_logic_vector(31 downto 0);
+SIGNAL Result: std_logic_vector(31 downto 0);
+
+
+
 
 BEGIN 
 
-U1:  full_adder_32 PORT MAP (PCSrc,"0100",PCPlus4);
+SignImm <= std_logic_vector(resize(signed(Instruction(15 downto 0)), SignImm'length));
+
+U1:  full_adder_32 PORT MAP (?,"0100",PCPlus4);
 U2:  mux32 PORT MAP ((PCPlus4(31 downto 28)) & (Instruction(25 downto 0) sll 2),PCNextbr,Jump,PCNext);
 U3:  full_adder_32 PORT MAP (PCPlus4,(SignImm sll 2),PCBranch);
-U4:  mux32 PORT MAP ((PCPlus4,PCBranch,PCScr,PCNextbr);
-U5:  Controller PORT MAP (Instruction(31 downto 26),(Instruction(31 downto 26),Zero,MemtoReg, MemWrite, MemRead,PCSrc, AluSrc ,RegDst, RegWrite, Jump ,AluControl)--pcSrc pas sur
+U4:  mux32 PORT MAP ((PCPlus4,PCBranch,PCSrc,PCNextbr);
 U6:  mux32 GENERIC MAP (N => 5) PORT MAP (Instruction(20 downto 16),Instruction(15 downto 11),RegDst,WriteReg);
-U7:  regfile PORT MAP(clk,RegWrite,Instruction(25 downto 21),Instruction(20 downto 16),WriteReg,result,SrcA,rd2);
+U7:  regfile PORT MAP(clk,RegWrite,Instruction(25 downto 21),Instruction(20 downto 16),WriteReg,Result,SrcA,rd2);
 U8:  mux32 PORT MAP (rd2,SignImm,ALUSrc,SrcB);
-U9: alu_32 PORT MAP (SrcA,SrcB,AluControl,open,Result,Zero);
+U9:  alu_32 PORT MAP (SrcA,SrcB,AluControl,open,AluResult,Zero);
+U10: mux32 PORT MAP (ReadData,rd2,MemtoReg,Result);
+U11: DFF PORT MAP (PCNext,clk,reset,PC);
 
 end DataPath_archi;
 
