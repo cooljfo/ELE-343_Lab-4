7d11c405d8cd5eebe2b669d39c3110c314a887da
chrisdsa <christian_dsa@hotmail.com>
2015-11-11 13:44:14 -0500

    Complete MIPS

--- MIPS.vhd
+++ MIPS.vhd
@@ -18,7 +18,28 @@ entity mips is
 end;
 
 architecture rtl of mips is
+COMPONENT datapath  
+  PORT (
+    clk         : in  std_logic;
+    reset       : in  std_logic;
+    instruction : in  std_logic_vector(31 downto 0);
+    data        : in  std_logic_vector(31 downto 0);
+    controlBus  : in  controlBus_t;
+
+    pc          : out unsigned(31 downto 0);
+    zero        : out std_logic;
+    result      : out std_logic_vector(31 downto 0);
+    regToData   : out std_logic_vector(31 downto 0)
+  ); END COMPONENT;
+
+COMPONENT controller 
+  PORT (
+    zero        : in  std_logic;
+    instruction : in  std_logic_vector(31 downto 0);
+    controlBus  : out controlBus_t
+  ); END COMPONENT;
 
 begin
 
 end;
+
